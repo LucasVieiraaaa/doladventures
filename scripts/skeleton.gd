a92c0288a5ac31005bba92ff587ef42698729450
LucasVieiraaaa <lucasvieirasouzaa@gmail.com>
2026-08-21 21:35:34 -0300

    spinning bones logic

--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -2,20 +2,25 @@ extends CharacterBody2D
 
 enum SkeletonState {
 	walk,
-	dead
+	dead,
+	attack
 }
+
+const SPINNING_BONE = preload("uid://rprq2p5g3r0w")
 
 var status : SkeletonState
 
 const SPEED = 10.0
 const JUMP_VELOCITY = -400.0
 var direction = 1
+var can_throw = true
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Hitbox
 @onready var wall_detector: RayCast2D = $WallDetector
 @onready var ground_detector: RayCast2D = $GroundDetector
-
+@onready var player_detector: RayCast2D = $PlayerDetector
+@onready var bone_start_position: Node2D = $BoneStartPosition
 
 func _physics_process(delta: float) -> void:
 
@@ -27,6 +32,9 @@ func _physics_process(delta: float) -> void:
 			walk_state(delta)
 		SkeletonState.dead:
 			dead_state(delta)
+		SkeletonState.attack:
+			attack_state(delta)
+			
 	move_and_slide()
 	
 func _ready() -> void:
@@ -49,10 +57,34 @@ func walk_state(_delta):
 		scale.x *= -1
 		direction *= -1
 		
-	
+	if player_detector.is_colliding():
+		go_to_attack_state()
+		return
+		
 func dead_state(_delta):
 	pass
 	
 func take_damage():
 	go_to_dead_state();
 	
+func go_to_attack_state():
+	status = SkeletonState.attack
+	anim.play("attack")
+	velocity = Vector2.ZERO
+	can_throw = true;
+	
+func attack_state(_delta):
+	if anim.frame == 2 && can_throw:
+		throw_bone()
+		can_throw = false;
+
+func throw_bone():
+	var new_bone =  SPINNING_BONE.instantiate()
+	add_sibling(new_bone)
+	new_bone.position = bone_start_position.global_position
+	new_bone.set_direction(self.direction)
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if anim.animation == "attack":
+		go_to_walk_state()
+		return
