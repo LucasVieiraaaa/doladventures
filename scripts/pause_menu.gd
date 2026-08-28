c52b6a4941eab72efd5f6a30d85aadca2c887ff7
LucasVieiraaaa <lucasvieirasouzaa@gmail.com>
2026-08-28 19:55:52 -0300

    adicionando main menu button

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+@onready var bamboo_sound: AudioStreamPlayer = $Sounds/BambooSound
+@onready var fade_out: ColorRect = $FadeOut
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,3 +22,16 @@ func _input(_event: InputEvent) -> void:
 func _on_button_pressed() -> void:
 	visible = false
 	get_tree().paused = false
+	
+
+func _on_main_menu_pressed() -> void:
+	bamboo_sound.play()
+
+	var tween := create_tween()
+	tween.tween_property(fade_out, "modulate:a", 1.0, 0.5)
+
+	await tween.finished
+
+	await get_tree().create_timer(0.7).timeout
+
+	get_tree().change_scene_to_file("res://scene/title_screen.tscn")
--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -5,6 +5,7 @@ func _ready() -> void:
 	pass
 
 func _on_start_pressed() -> void:
+	print("apertou")
 	bamboo_sound.play()
 	await get_tree().create_timer(0.09).timeout
 	get_tree().change_scene_to_file("res://scene/tropic.tscn")
