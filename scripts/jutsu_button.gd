d120e9359f3ce2a5116a45a87445cc7e5c5f1c57
Lucas Souza <lucas.souza@aptasistemas.com.br>
2026-08-31 21:41:24 -0300

    ajuste, agora o botão está no padrão placeholder

--- scripts/jutsu_button.gd
+++ scripts/jutsu_button.gd
@@ -4,27 +4,34 @@ extends TextureButton
 @onready var key: Label = $Key
 @onready var time: Label = $Time
 @onready var timer: Timer = $Timer
-	
+
 var jutsu = null
-	
-var change_key = "":
+
+var change_key := "":
 	set(value):
-		change_key =  value
+		change_key = value
 		key.text = value
-		shortcut = Shortcut.new()
-		var input_key = InputEventKey.new()
-		input_key.keycode = value	.unicode_at(0)
-		
-		shortcut.events = [input_key]
-		
+
+
 func _ready():
+	tooltip_text = ""
+	shortcut = null
+
 	change_key = "Q"
 	cooldown.max_value = timer.wait_time
 	set_process(false)
-	
+
 func _process(_delta: float):
-		time.text = "%3.1f" % timer.time_left
-		cooldown.value = timer.time_left
+	time.text = "%3.1f" % timer.time_left
+	cooldown.value = timer.time_left
+
+
+func _input(event: InputEvent):
+	if event is InputEventKey:
+		if event.pressed and not event.echo:
+			if event.keycode == change_key.unicode_at(0):
+				if not disabled:
+					_on_pressed()
 
 
 func _on_pressed() -> void:
